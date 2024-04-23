41ba858f8bfacd9ac4186be92cade1b506d9bc0b
Luke Hunter <luke.hunter9@gmail.com>
2024-04-23 17:45:29 +1000

    Added questions to array

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'Project Title',
+    'Description',
+    'Installation',
+    'Usage',
+    'Credits',
+    'License',
+    'Badges',
+    'Features',
+    'How to Contribute',
+    'Tests'
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
