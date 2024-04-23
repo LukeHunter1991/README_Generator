9c5f1a2783cb35378dd9b463b9e9da9042474eb3
Luke Hunter <luke.hunter9@gmail.com>
2024-04-23 17:59:30 +1000

    Completed WriteToFile function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,7 +17,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log('README created!')
+    })
+ }
 
 // TODO: Create a function to initialize app
 function init() { }
